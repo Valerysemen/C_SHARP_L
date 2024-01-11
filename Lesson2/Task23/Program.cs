39f1790560343bf20267fd29b8eff3c967f373f5
Valery <6488671s@gmail.com>
2024-01-11 17:44:18 +0300

    add lesson2:task23

---
+++ Lesson2/Task23/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = 10;
+int[] array = { 2, 95, 4, 7, 8, 35, 1, 3, 8, 6 };
+int i = 0;
+int max = array[0];
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+
+
+
+
