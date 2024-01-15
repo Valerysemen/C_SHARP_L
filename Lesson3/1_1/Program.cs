c89a2ecfc159b5d827f27279573c0fd17c5ea323
Valery <6488671s@gmail.com>
2024-01-15 14:59:46 +0300

    add lesson3:task1

---
+++ Lesson3/1_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// 1. Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+Console.WriteLine("Введите значение");
+int a = int.Parse(Console.ReadLine()!);
+
+int[] arr = { 1, 2, 3, 4, 5 };
+int i = 0;
+bool found = false;
+
+while (i < arr.Length)
+{
+if (a == arr[i])
+{
+found = true;
+break;
+}
+i = i + 1;
+}
+
+if (found)
+{
+Console.WriteLine("Da");
+}
+else
+{
+Console.WriteLine("Net");
+}
