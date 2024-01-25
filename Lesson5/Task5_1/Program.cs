1b8be9407beb9a372e9ce3c6a8062a01b7846ebb
Valery <6488671s@gmail.com>
2024-01-25 15:43:07 +0300

    add lesson5:Task5_1

---
+++ Lesson5/Task5_1/Program.cs
@@ -0,0 +1,22 @@
+﻿int Fact(int n)       // Рекурсивные способы поиска факториала
+{                     //на практиктике не используется
+    if (n == 1 || n == 0) // Используется цикл для этого (на п -1) итераций
+    {
+        Console.WriteLine($"STOP requrson:n={n}"); //отладочный вывод на экран
+        return 1;
+    }
+    Console.WriteLine(n);// Выводим параметры на экран
+    return n * Fact(n - 1);  //вычисление Факториала
+}
+Console.Write(Fact(5));
+
+//int Fact(int n)
+//{
+//    int result = 1;
+//    for(int i =2; i <= n; i++) 
+//    {
+//        result *= i;
+//    }
+//    return result;
+//}
+//Console.Write(Fact(2));
